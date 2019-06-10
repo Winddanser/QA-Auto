e0906bd2c8bb2e1ebb9eb1c845d0c4800a03d406
Winddanser <winddanser@gmail.com>
2019-06-10 21:30:54 +0300

    Add Tests Sessions

--- src/main/java/ua/kiev/prog/automation/framework/app/forum/blocks/category/BoardItemWidget.java
+++ src/main/java/ua/kiev/prog/automation/framework/app/forum/blocks/category/BoardItemWidget.java
@@ -4,14 +4,12 @@ import ua.kiev.prog.automation.framework.base.Locator;
 import ua.kiev.prog.automation.framework.base.Widget;
 import ua.kiev.prog.automation.framework.base.widget.GUILink;
 
-public class BoardItemWidget extends Widget
-{
+public class BoardItemWidget extends Widget {
     public BoardItemWidget(Locator locator) {
         super(locator);
     }
 
-    final public GUILink name ()
-    {
+    final public GUILink name() {
         return new GUILink(this.locator().createChild("./td[@class='info']//a"));
     }
 }
